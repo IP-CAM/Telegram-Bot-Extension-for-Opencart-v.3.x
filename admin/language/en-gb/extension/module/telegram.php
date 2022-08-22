72ac4b4fce028d8a81b19c9d564e55725360943a
SDG1212 <sokol1294@gmail.com>
2022-08-22 21:21:34 +0300

    Add Telegram Module

---
+++ admin/language/en-gb/extension/module/telegram.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    = 'Telegram';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified telegram module!';
+$_['text_edit']        = 'Edit Telegram Module';
+
+// Entry
+$_['entry_bot']        = 'Bot ID';
+$_['entry_chat']       = 'Chat ID';
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify telegram module!';
+$_['error_bot']        = 'Bot ID is required!';
+$_['error_chat']       = 'Chat ID is required!';
---
+++ controller/extension/module/telegram.php
@@ -0,0 +1,49 @@
+<?php
+class ControllerExtensionModuleTelegram extends Controller {
+	public function send($text) {
+    if (!$this->config->get('module_telegram_status')) {
+      return;
+    }
+
+		$json = array();
+
+		$bot_token = $this->config->get('module_telegram_bot');
+
+		$api_url = 'https://api.telegram.org/bot' . $bot_token . '/sendMessage';
+
+		$handle = curl_init($api_url);
+
+		$parameters = array(
+			'chat_id' => $this->config->get('module_telegram_chat'),
+			'text'    => $text
+		);
+
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 10);
+		curl_setopt($handle, CURLOPT_TIMEOUT, 10);
+		curl_setopt($handle, CURLOPT_POST, 1);
+		curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($parameters));
+		curl_setopt($handle, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+		$response = curl_exec($handle);
+
+		if (curl_error($handle)) {
+			$this->log->write('Telegram Bot CURL ERROR: ' . curl_errno($handle) . '::' . curl_error($handle));
+		} elseif (!$response) {
+			$this->log->write('Telegram Bot CURL ERROR: Empty Gateway Response');
+		}
+
+		curl_close($handle);
+
+		$result = json_decode($response, true);
+
+		if (!$result['ok']) {
+			$this->log->write(json_encode($response));
+
+			$json['error'] = $this->language->get('error_unexpected');
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
+}
